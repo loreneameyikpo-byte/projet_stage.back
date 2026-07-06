558eeff71bae6e61ce3d98503560daf5227472e5
Lorene <lorene.ameyikpo@gmail.com>
2026-07-06 14:15:54 +0000

    Création des migrations

---
+++ database/migrations/create_promotion_table.php
@@ -0,0 +1,27 @@
+<?php
+ use Illuminate\Database\Migrations\Migration;
+ use Illuminate\Database\Schema\Blueprint;  
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotion', function (Blueprint $table) {
+            $table->id('id_promotion');
+            $table->year('annee');
+            $table->foreignId('id_niveau')->constrained('niveau', 'id_niveau');            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotion');
+    }
+}
---
+++ database/migrations/create_roles_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+{
+    Schema::create('role', function (Blueprint $table) {
+        $table->id('id_role');
+        $table->string('libelle');
+        $table->timestamps();
+    });
+}
+
+public function down(): void
+{
+    Schema::dropIfExists('role');
+}
+
+};
